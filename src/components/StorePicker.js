8d9c0d4fd449d9b443de6c17f6d8dceff2797518
Johnathan Lyman <jelyman@gmail.com>
2017-03-08 18:12:16 -0800

    video 11

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,12 +2,24 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  // constructor() {
+  //   super();
+  //   this.goToStore = this.goToStore.bind(this);
+  // }
+  goToStore(event) {
+    event.preventDefault();
+    console.log('You changes the URL');
+    // first grab the test from the box
+    console.log(this.storeInput.value);
+    // second we're going to transition from / to /store/:storeId
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
         { /* This is a comment */ }
         <h2>Please Enter a Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => {this.storeInput = input}} />
         <button type="submit">Visit Store</button>
       </form>
     )
